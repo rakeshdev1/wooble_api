f5689c102128d3c01194d214487c533d01158793
Akash Jaiswal <akashjas1996@gmail.com>
2023-01-03 15:40:35 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,11 +1,14 @@
 <?php
-$server="localhost";
-$username="root";
-$password="";
-$database = "wooble";
+// Create 4 variables to store these information
+$server="217.21.80.10";
+$username="u502039025_h85";
+$password="Tp9M!o7Vc?";
+$database = "u502039025_h85";
+// Create connection
 $conn = new mysqli($server, $username, $password, $database);
+// Check connection
+//echo "Connected";
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
   echo "Problem";
 }
-?>
