2088f97b1bd15a4f73d0bf9f6a7bf84aaec6953e
rakesh <rakeshsutar85@gmail.com>
2023-02-06 17:42:32 +0530

    updated

--- profile.php
+++ profile.php
@@ -2,11 +2,19 @@
 require_once "conn.php";
 require_once "validate.php";
 define('UPLOAD_PATH', '../home/images/');
+//define('UPLOAD_PATH', 'upload/');
+//define('DB_HOST', 'localhost');
+//define('DB_USER', 'root');
+//define('DB_PASS', '');
+//define('DB_NAME', 'wooble');
+$url = 'https://' . 'app.wooble.org/home/images/';
+//$url = 'http://172.168.0.182/wooble_api/upload/';
+//$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME) or die('Unable to connect');
 $response = array();
 if (isset($_GET['apicall'])) {
     switch ($_GET['apicall']) {
         case 'updatefullprofile':
-            if (isset($_FILES['pic']['name']) || isset($_POST['fullname']) || isset($_POST['username'])) {
+            if (isset($_FILES['pic']['name']) || isset($_POST['username'])) {
                 $profileEmail = validate($_POST['profileEmail']);
                 $fullname = validate($_POST['fullname']);
                 $profile_pic = $_FILES['pic']['name'];
@@ -18,7 +26,23 @@ if (isset($_GET['apicall'])) {
                 $twitter_link = validate($_POST['twitter_link']);
                 $whatsapp_link = validate($_POST['whatsapp_link']);
                 $background = validate($_POST['background']);
+
+                $cover_pic_name = "";
+
                 try {
+
+                    $stmt1 = $conn->prepare("SELECT `compressed_image`FROM `user_details` WHERE email=?");
+                    $stmt1->bind_param("s", $_POST['profileEmail']);
+                    $stmt1->execute();
+                    $stmt1->bind_result($cover_pic_name_select);
+                    while ($stmt1->fetch()) {
+                        $cover_pic_name = $cover_pic_name_select;
+                    }
+
+                    if ($cover_pic_name != null) {
+                        unlink(UPLOAD_PATH . $cover_pic_name);
+                    }
+
                     move_uploaded_file($_FILES['pic']['tmp_name'], UPLOAD_PATH . $_FILES['pic']['name']);
                     $stmt = $conn->prepare("UPDATE user_details SET field_1=?,compressed_image=?,username=?,field_2=?,fb_link=?,insta_link=?,linkedin_link=?,twitter_link=?,whatsapp_link=?,field_3=? WHERE email=? ");
                     $stmt->bind_param("sssssssssss", $fullname, $profile_pic, $username, $designation, $fb_link, $insta_link, $linkedin_link, $twitter_link, $whatsapp_link, $background, $profileEmail);
@@ -45,12 +69,12 @@ if (isset($_GET['apicall'])) {
             $stmt = $conn->prepare("SELECT field_1, compressed_image, username, field_2, fb_link, insta_link, linkedin_link, twitter_link, whatsapp_link, field_3 FROM user_details WHERE email=?");
             $stmt->bind_param("s", $profileEmail);
             $stmt->execute();
-            $stmt->bind_result($fullname, $profile_pic, $username, $designation, $fb_link, $insta_link, $linkedin_link, $twitter_link, $whatsapp_link, $background);
+            $stmt->bind_result($fullname, $compressed_image, $username, $designation, $fb_link, $insta_link, $linkedin_link, $twitter_link, $whatsapp_link, $background);
             $images = array();
             while ($stmt->fetch()) {
                 $temp = array();
                 $temp['fullname'] = $fullname;
-                $temp['profile_pic'] = 'http://' . $server_ip . '/wooble-api/' . UPLOAD_PATH . $profile_pic;
+                $temp['profile_pic'] = $url . $compressed_image;
                 $temp['username'] = $username;
                 $temp['designation'] = $designation;
                 $temp['fb_link'] = $fb_link;
@@ -79,4 +103,3 @@ if (isset($_GET['apicall'])) {
     echo "The page that you have requested could not be found.";
     exit();
 }
-?>
