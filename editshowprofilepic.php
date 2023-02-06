f4759af0f435893ae3b3056a2d882564923582e7
rakesh <rakeshsutar85@gmail.com>
2023-02-06 16:01:39 +0530

    updated

--- editshowprofilepic.php
+++ editshowprofilepic.php
@@ -2,6 +2,14 @@
 require_once "conn.php";
 require_once "validate.php";
 define('UPLOAD_PATH', 'home/images/');
+// define('UPLOAD_PATH', 'upload/');
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'wooble');
+ $url = 'https://' . 'wooble.org/app/home/images/';
+//$url = 'http://172.168.0.182/wooble_api/upload/';
+//$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME) or die('Unable to connect');
 $response = array();
 $profileEmail = $_POST['profileEmail'];
 if (isset($_GET['apicall'])) {
@@ -9,19 +17,22 @@ if (isset($_GET['apicall'])) {
         case 'updateprofilepic':
             if (isset($_FILES['pic']['name']) && isset($_POST['profileEmail'])) {
                 try {
-                    $profile_pic_name = "";
+                    $cover_pic_name = "";
                     $stmt1 = $conn->prepare("SELECT `compressed_image`FROM `user_details` WHERE email=?");
                     $stmt1->bind_param("s", $_POST['profileEmail']);
                     $stmt1->execute();
-                    $stmt1->bind_result($profile_pic_name_select);
+                    $stmt1->bind_result($cover_pic_name_select);
                     while ($stmt1->fetch()) {
-                        $profile_pic_name = $profile_pic_name_select;
+                        $cover_pic_name = $cover_pic_name_select;
                     }
-                    unlink(UPLOAD_PATH . $profile_pic_name);
 
+                    if ($cover_pic_name != null) {
+                        unlink(UPLOAD_PATH . $cover_pic_name);
+                    }
+                    $cover_pic_name = $_FILES['pic']['name'];
                     move_uploaded_file($_FILES['pic']['tmp_name'], UPLOAD_PATH . $_FILES['pic']['name']);
-                    $stmt = $conn->prepare("UPDATE user_details SET compressed_image=? WHERE email=? ");
-                    $stmt->bind_param("ss", $_FILES['pic']['name'], $_POST['profileEmail']);
+                    $stmt = $conn->prepare("UPDATE `user_details` SET `compressed_image`=? WHERE `email`=? ");
+                    $stmt->bind_param("ss", $cover_pic_name, $_POST['profileEmail']);
                     if ($stmt->execute()) {
                         $response['error'] = false;
                         $response['message'] = 'File uploaded successfully';
@@ -36,24 +47,40 @@ if (isset($_GET['apicall'])) {
                 $response['error'] = true;
                 $response['message'] = "Required params not available";
             }
+            echo json_encode($response);
 
             break;
         case 'getprofilepic':
-            $server_ip = gethostbyname(gethostname());
-            $stmt = $conn->prepare("SELECT compressed_image FROM user_details WHERE email='$profileEmail' ");
-            $stmt->execute();
-            $stmt->bind_result($image);
-            $images = array();
-            while ($stmt->fetch()) {
-                $temp = array();
-                $temp['image'] = 'https://' . 'wooble.org/app/home/images/' . $image;
-                array_push($images, $temp);
+            if (isset($_POST['profileEmail'])) {
+                try {
+                    $stmt = $conn->prepare("SELECT `compressed_image` FROM `user_details` WHERE `email`=? ");
+                    $stmt->bind_param("s", $_POST['profileEmail']);
+                    $stmt->execute();
+                    $stmt->bind_result($image);
+                    $imagelink = $image;
+                    echo $imagelink;
+                    $images = array();
+                    while ($stmt->fetch()) {
+                        $temp = array();
+                        $temp['image'] = $url . $image;
+                        array_push($images, $temp);
+                    }
+                    $response['error'] = false;
+                    $response['images'] = $images;
+
+                    header('Content-Type: application/json');
+                    echo json_encode($images);
+
+                } catch (Exception $e) {
+                    $response['error'] = true;
+                    $response['message'] = 'Could not upload file';
+                }
+            } else {
+                $response['error'] = true;
+                $response['message'] = "Required params not available";
             }
 
-            $response['error'] = false;
-            $response['images'] = $images;
             break;
-
         default:
             $response['error'] = true;
             $response['message'] = 'Invalid api call';
@@ -64,7 +91,5 @@ if (isset($_GET['apicall'])) {
     echo "The page that you have requested could not be found.";
     exit();
 }
-
 header('Content-Type: application/json');
-echo json_encode($images);
-?>
+//echo json_encode($images);
